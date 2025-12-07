38662dce85ec6e29b891926c0144f2367e3b810d
Rajeev Kumar Maurya <mauryarajeev560@gmail.com>
2025-12-07 18:15:40 +0000

    conversionoperation

---
+++ 03_conversionoperations.js
@@ -0,0 +1,22 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+let isLoggedIn="hitesh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
+
+
+
+
+let someNumber=33
+let stringNumber=String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);

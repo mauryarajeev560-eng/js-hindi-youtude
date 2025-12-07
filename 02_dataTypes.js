5ee132e98d0834dd3b703f06ecb6c247e411b422
Rajeev Kumar Maurya <mauryarajeev560@gmail.com>
2025-12-07 16:35:53 +0000

    datatpes

---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs,not browser
+console.log(3+3); console.log("Rajeev")
+let age=18
+let isLoggedIn=false
+let state;
+
+//number=>2 to power53 
+//bigint
+//bigint
+//boolean=> standlone value
+//undefined=>
+//symbol =>unique
+
+//object
+
+
+console.log(typeof null); 
